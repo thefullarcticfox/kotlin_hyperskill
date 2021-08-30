92e365ea3ca2eaed88d2329e8b9b2af2311767dd
Ivan <thefullarcticfox@users.noreply.github.com>
2021-08-30 14:30:33 +0300

    Upgraded Gradle to 7.0.1 and Basic Calculator init

---
+++ basic-calculator/app/src/main/java/com/thefullarcticfox/basiccalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thefullarcticfox.basiccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
